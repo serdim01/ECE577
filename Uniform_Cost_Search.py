5ab4cd0ad68434ab04fe154b5050494e2e9c508a
charlesjberg <47033366+charlesjberg@users.noreply.github.com>
2022-10-06 17:00:14 -0400

    Add files via upload

--- Uniform_Cost_Search.py
+++ Uniform_Cost_Search.py
@@ -4,190 +4,183 @@ Created on Thu Sep 22 14:12:49 2022
 
 @author: CJ
 """
-# https://stackoverflow.com/questions/2482602/a-general-tree-implementation
-import queue
-       
-class TreeNode(object):
-    def __init__(self, name):
+import csv
+import time
+from operator import attrgetter  
+    
+class city_US(object):
+    def __init__(self, name, number, state):
         self.Name = name
         self.children = dict()
-
+        self.Number = number
+        self.State = state
     def add_child(self, name, cost):
         self.children[name] = cost
 
-
+class priorityQueueObject(object):
+    def __init__(self, name, pathcost):
+        self.Number = name
+        self.PathCost = pathcost
         
-def createRomaniaTree():
-    mapDict = dict();
-    Arad = TreeNode("Arad")
-    Arad.add_child("Zerind", 75)
-    Arad.add_child("Timisoara", 118)
-    Arad.add_child("Sibiu", 140)
-    mapDict["Arad"] = Arad    
+def readinTowns():
+    cityDict = dict()
+    # Create all entries of Dict with txt file with all city names
+    # Dict keys will be a unique number identifier to avoid cities w/ same name in diff states
+    with open("C:/Users/CJ/Documents/Grad_2022-23/ECE577/Project1/sf12010placename.txt") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                StateNumber = int(row[0])*100000 + int(row[2])
+                StateNumberStr = str(StateNumber).zfill(7)
+                cityDict[StateNumberStr] = city_US(row[3], StateNumberStr, row[1])
+    # Add adjacent towns within 25 mile radius of each town in dict            
+    with open("C:/Users/CJ/Documents/Grad_2022-23/ECE577/Project1/sf12010placedistance25miles.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                StateNumber1 = int(row[0])*100000 + int(row[1])
+                StateNumberStr1 = str(StateNumber1).zfill(7)
+                StateNumber2 = int(row[3])*100000 + int(row[4])
+                StateNumberStr2 = str(StateNumber2).zfill(7)
+                cityDict[StateNumberStr1].add_child(StateNumberStr2, row[2])    
+    return cityDict
 
-    Zerind = TreeNode("Zerind")
-    Zerind.add_child("Arad", 75)
-    Zerind.add_child("Oradea", 71)
-    mapDict["Zerind"] = Zerind
-    
-    Oradea = TreeNode("Oradea")
-    Oradea.add_child("Sibiu", 151)
-    Oradea.add_child("Zerind", 71)
-    mapDict["Oradea"] = Oradea
-    
-    Sibiu = TreeNode("Sibiu")
-    Sibiu.add_child("Oradea", 151)
-    Sibiu.add_child("Fagaras", 99)
-    Sibiu.add_child("Arad", 140)
-    Sibiu.add_child("Rimnicu_Vilcea", 80)
-    mapDict["Sibiu"] = Sibiu    
+# https://stackoverflow.com/questions/41760856/most-simple-tree-data-structure-in-python-that-can-be-easily-traversed-in-both
+class Tree(object):
+    def __init__(self, data, pathcost, children=None, parent=None):
+        self.data = data
+        self.PathCost = pathcost
+        self.children = children or []
+        self.parent = parent
 
-    Fagaras = TreeNode("Fagaras")
-    Fagaras.add_child("Bucharest", 211)
-    Fagaras.add_child("Sibiu", 99)
-    mapDict["Fagaras"] = Fagaras
+    def add_child(self, data, pathCost):
+        new_child = Tree(data, pathCost, parent=self)
+        self.children.append(new_child)
+        return new_child
 
-    Bucharest = TreeNode("Bucharest")
-    Bucharest.add_child("Fagaras", 211)
-    Bucharest.add_child("Giurgiu", 90)
-    Bucharest.add_child("Pitesti", 101)
-    Bucharest.add_child("Urziceni", 85)    
-    mapDict["Bucharest"] = Bucharest
+    def is_root(self):
+        return self.parent is None
 
-    Urziceni = TreeNode("Urziceni")
-    Urziceni.add_child("Vaslui", 142)
-    Urziceni.add_child("Hirsova", 98)
-    Urziceni.add_child("Bucharest", 85)    
-    mapDict["Urziceni"] = Urziceni     
+    def is_leaf(self):
+        return not self.children
 
-    Hirsova = TreeNode("Hirsova")
-    Hirsova.add_child("Eforie", 86)
-    Hirsova.add_child("Urziceni", 98)   
-    mapDict["Hirsova"] = Hirsova 
-
-    Eforie = TreeNode("Eforie")
-    Eforie.add_child("Hirsova", 86)  
-    mapDict["Eforie"] = Eforie
-
-    Vaslui = TreeNode("Vaslui")
-    Vaslui.add_child("Urziceni", 142)
-    Vaslui.add_child("Iasi", 92)   
-    mapDict["Vaslui"] = Vaslui
-
-    Iasi = TreeNode("Iasi")
-    Iasi.add_child("Neamt", 87)
-    Iasi.add_child("Vaslui", 92)   
-    mapDict["Iasi"] = Iasi   
-
-    Neamt = TreeNode("Neamt")
-    Neamt.add_child("Iasi", 87)  
-    mapDict["Neamt"] = Neamt
-
-    Giurgiu = TreeNode("Giurgiu")
-    Giurgiu.add_child("Bucharest", 90)  
-    mapDict["Giurgiu"] = Giurgiu
-
-    Pitesti = TreeNode("Pitesti")
-    Pitesti.add_child("Craiova", 138)
-    Pitesti.add_child("Rimnicu_Vilcea", 97)  
-    Pitesti.add_child("Bucharest", 101)  
-    mapDict["Pitesti"] = Pitesti
-    
-    Rimnicu_Vilcea = TreeNode("Rimnicu_Vilcea")
-    Rimnicu_Vilcea.add_child("Craiova", 146)
-    Rimnicu_Vilcea.add_child("Pitesti", 97)  
-    Rimnicu_Vilcea.add_child("Sibiu", 80)  
-    mapDict["Rimnicu_Vilcea"] = Rimnicu_Vilcea
-    
-    Craiova = TreeNode("Craiova")
-    Craiova.add_child("Rimnicu_Vilcea", 146)
-    Craiova.add_child("Pitesti", 138)  
-    Craiova.add_child("Drobeta", 120)  
-    mapDict["Craiova"] = Craiova
- 
-    Drobeta = TreeNode("Drobeta")
-    Drobeta.add_child("Craiova", 120)
-    Drobeta.add_child("Mehadia", 75)   
-    mapDict["Drobeta"] = Drobeta
-    
-    Mehadia = TreeNode("Mehadia")
-    Mehadia.add_child("Drobeta", 75)
-    Mehadia.add_child("Lugoj", 70)   
-    mapDict["Mehadia"] = Mehadia
-
-    Lugoj = TreeNode("Lugoj")
-    Lugoj.add_child("Mehadia", 70)
-    Lugoj.add_child("Timisoara", 111)   
-    mapDict["Lugoj"] = Lugoj
-
-    Timisoara = TreeNode("Timisoara")
-    Timisoara.add_child("Arad", 70)
-    Timisoara.add_child("Lugoj", 111)   
-    mapDict["Timisoara"] = Timisoara
-    
-    return mapDict
-
+    def __str__(self):
+        if self.is_leaf():
+            return str(self.data)
+        return '{data} [{children}]'.format(data=self.data, children=', '.join(map(str, self.children)))   
 
 def main():
-    mapGraph = createRomaniaTree()
-    frontier = queue.Queue()
-    pathCost = queue.Queue()
-    visited = [];    
-    start = "blah"
-    while (not(start in mapGraph)):
-        print("Enter start city:" )
-        start = input()
-        if not(start in mapGraph):
-            print("Invalid city name!")
+    # cityGraph is our problem space - all 
+    cityGraph = readinTowns()
+    frontier = []
+    frontier_dict = dict()
+    visited_dict = dict()
     
-    goal = "blah"
-    while (not(goal in mapGraph)):
-        print("Enter destination:" ) 
-        goal = input()        
-        if not(goal in mapGraph):
+    # Get user input for start and goal
+    start = 0
+    while (start == 0):
+        print("Enter start state:" )
+        start_state = input()
+        print("Enter start city:")
+        start_city = input()
+        for city in cityGraph:
+            if ((cityGraph[city].Name == start_city) and (cityGraph[city].State == start_state)):
+                start = cityGraph[city].Number
+        if (start == 0):
+            print("Invalid city name!")    
+    goal = 0
+    while (goal == 0):
+        print("Enter destination state:" )
+        goal_state = input()
+        print("Enter destination city:")
+        goal_city = input()
+        # Initial Goal Check
+        for city in cityGraph:
+            if ((cityGraph[city].Name == goal_city) and (cityGraph[city].State == goal_state)):
+                goal = cityGraph[city].Number
+        if (goal == 0):
             print("Invalid city name!")
-        if start == goal:
-            print("You are already here!")
+        
+    st = time.time() 
+    frontier = [priorityQueueObject(cityGraph[start].Number, 0)];      
+    current_state = frontier[0]
+    frontier_dict[current_state.Number] = Tree(current_state.Number, 0)
+    
+     # Goal Check - Loop until we reach goal
+    while(current_state.Number != goal):
+        # Explore on current level - kids is the city ID number [string]
+        # Iterate through adjacent towns fro current state
+        for kids in cityGraph[current_state.Number].children:
+            # Check if the adjacent towns is in visited dict or frontier dict
+            # Only add if not in visited or frontier 
+            # If is in frontier with higher cost - replace
+            if not(kids in visited_dict.keys()):
+                if (kids in frontier_dict.keys()):
+                    pathCostInFrontier = frontier_dict[kids].PathCost
+                    pathCostToChild = current_state.PathCost + float(cityGraph[current_state.Number].children[kids])
+                    if (pathCostInFrontier > pathCostToChild):
+                        for x in frontier:
+                            if x.PathCost == pathCostInFrontier:
+                                break
+                        frontier.remove(x)
+                        frontier.append(priorityQueueObject(kids,pathCostToChild ))
+                        del frontier_dict[kids]
+                        frontier_dict[kids] = frontier_dict[current_state.Number].add_child(kids, pathCostToChild)
+                elif not(kids in frontier_dict.keys()):
+                    # Add children to current state frontier dict
+                    # Children are themselves a tree node
+                    pathCostToChild = current_state.PathCost + float(cityGraph[current_state.Number].children[kids])
+                    frontier_dict[kids] = frontier_dict[current_state.Number].add_child(kids, pathCostToChild)
+                    frontier.append(priorityQueueObject(kids,pathCostToChild ))
+                    
+        # Add current state to visited - visited is the main tree we are creating            
+        visited_dict[current_state.Number] = frontier_dict[current_state.Number] 
+        frontier.remove(current_state)
+        del frontier_dict[current_state.Number]
+        nextState = min(frontier,key=attrgetter('PathCost'))
+        
+        # If no more items in our queue then we cannot reach goal
+        if len(frontier) == 0:
+            print("No possible route")
             return
         
+        # Dequeue Current state and delete from fronteir dict        
+        current_state = nextState  
     
-    frontier = [mapGraph[start]];      
-    current_state = frontier.pop()
-    pathCost = [0]
-    current_cost = pathCost.pop();
-    from_where = [start]
-    current_path = from_where.pop();
+        # Print excecution time
+    et = time.time()
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
     print()
-    # Loop until we reach goal state
-    while(current_state.Name != goal):
-        
-        # Explore on current level
-        print("CurrentState: " + current_state.Name)
-        print("Current Path Cost: " + str(current_cost))
-        for kids in current_state.children:
-            if not((kids) in visited):
-                print("Pushing: " + kids)
-                pathCost.append(current_cost + current_state.children[kids])
-                frontier.append(kids)
-                from_where.append(current_path + " -> " + kids)
-        # https://stackoverflow.com/questions/6618515/sorting-list-based-on-values-from-another-list        
-        frontier = [x for _,x in sorted(zip(pathCost,frontier), reverse=True)]
-        from_where = [x for _,x in sorted(zip(pathCost,from_where), reverse=True)]                               
-        # Add current site to visited list
-        visited.append(current_state.Name)
-        # Pop from frontier to get next state
-        current_state = mapGraph[frontier.pop()]
-        # Sort path costs, will be same order as frontier
-        pathCost.sort(reverse=True)
-        # Pop cost of path of current state 
-        current_cost = pathCost.pop()
-        current_path = from_where.pop()
-        print("Popping: " + current_state.Name)
-        print("Current path: " + current_path)
-
-    print()
-    print("Final Route = "+ current_path)
-    print("Final distance = " + str(current_cost))
-
+    
+    # Bookeeping - don't add goal to visited in loop - do now for graph traversal    
+    visited_dict[current_state.Number] = frontier_dict[current_state.Number]  
+    
+    # Use visited tree to find path found
+    state = current_state.Number
+    path_reverse_order = list()
+    while state != start:
+        path_reverse_order.append(cityGraph[state].Name + ", " + (cityGraph[state].State))
+        state = visited_dict[state].parent.data
+ 
+    # Loop won't add start
+    path_reverse_order.append(cityGraph[start].Name + ", " + (cityGraph[start].State))
+    # Reverse order as we started at goal
+    path_reverse_order.reverse()
+    # Print Route
+    for cities in path_reverse_order:
+        if cities == (goal_city + ", " + goal_state):
+            print(cities)
+        else:
+            print(cities + " -> ", end='')
+    print()        
+    print("Final distance = " + str(current_state.PathCost))
+    
 if __name__ == "__main__":
     main()
